35132ed77cbc22aa56a9e03c14cbc9625b5a7dbe
dmytro babinenko <tw1nystudy@gmail.com>
2024-04-03 10:02:08 +0300

    qwewqe

--- file.java
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-    var menu = document.getElementById('mymobile');
-    if (menu.style.display === 'none') {
-        menu.style.display = 'block'; 
-    } else {
-        menu.style.display = 'none'; 
-}
-}
