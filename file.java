bcda98479faee947db7bbf37400383cb92ba890e
dmytro babinenko <tw1nystudy@gmail.com>
2024-04-03 07:28:59 +0300

    123

---
+++ file.java
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    var menu = document.getElementById('mymobile');
+    if (menu.style.display === 'none') {
+        menu.style.display = 'block'; 
+    } else {
+        menu.style.display = 'none'; 
+}
+}
